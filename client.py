2848cb76d342129c2a5c30b8aabb03c5b05f19cb
Jay <jaysun1967@gmail.com>
2021-11-09 12:18:35 +1100

    account auth/creation works for 1 user

--- client.py
+++ client.py
@@ -1,9 +1,35 @@
 # Used sample code from TCPclient.py from webcms, https://webcms3.cse.unsw.edu.au/COMP3331/21T3/resources/66101,
 # and https://pythonprogramming.net/client-chatroom-sockets-tutorial-python-3/?completed=/server-chatroom-sockets-tutorial-python-3/
  
-import socket
+import socket 
 import sys
 import datetime
+
+# Funtion to authorise client
+def authorisation(client_socket):
+    # Authorise client's username
+    client_socket.send(username.encode())
+
+    # Check for server response after sending username
+    while True:
+        username_validation = client_socket.recv(1024).decode()
+        break
+
+    if username_validation == "username_validated":
+        password = input("Enter password: ")
+        # Send client's password for authorisation
+        client_socket.send(password.encode())
+
+    # Check for server response after sending password
+    while True:
+        password_validation = client_socket.recv(1024).decode()
+        break
+
+    # If password is validated, return true. else return false
+    if password_validation == "user_authorised":
+        return True
+    else:
+        return False
 
 # Finding given port number. Raise error if invalid port number.
 server_port = int(sys.argv[1])
@@ -11,15 +37,58 @@ if server_port == 80 or server_port == 8080 or server_port < 1024:
     print("Error: Use a standard port number")
     sys.exit(1)
 
-# Get user's username
-username = input("Enter username: ")
-
 # Creating socket and intially setting up the client
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost', server_port))
-client_socket.setblocking(False)
+client_socket.setblocking(1)
 
-print(f"User {username} connected to 127.0.0.1:{server_port}")
+# User authorisation
+username = input("Enter username: ")
+#auth = authorisation(client_socket)
+# Authorise client's username
+client_socket.send(username.encode())
+
+# Check for server response after sending username
+while True:
+    username_validation = client_socket.recv(1024).decode()
+    break
+
+auth = False
+attempts = 2
+if username_validation == "username_validated":
+    # Check for server response after sending password
+    while True:
+        password = input("Enter password: ")
+        # Send client's password for authorisation
+        client_socket.send(password.encode())
+
+        password_validation = client_socket.recv(1024).decode()
+
+        # If password is validated, return true. else return false
+        if password_validation == "user_authorised":
+            print(f"{username} successfully connected to 127.0.0.1:{server_port}")
+            break
+        # User gets only 3 attempts to enter the correct password
+        elif attempts > 0: 
+            print(f"Error: Incorrect password. Attempts remaining {attempts}!")
+            attempts = attempts - 1
+        else:
+            print("Error: password was entered incorrectly")
+            sys.exit()
+# If username does not exist, create a new account
+elif username_validation == "username_invalid":
+    print(f"{username} does not have an associated account, please enter a password to create an account")
+    password = input("Enter password: ")
+    # Send password to server
+    client_socket.send(password.encode())
+
+    account_creation = client_socket.recv(1024).decode()
+
+    if account_creation == "account_created":
+        print("Account successfully created!")
+    else:
+        print("Error: Account creation was unsuccessful!")
+        sys.exit()
 
 while True: 
     #message_recv = client_socket.recv(1024).decode()
@@ -27,7 +96,7 @@ while True:
 
     message_send = input("Enter message: ")
     date = datetime.datetime.now()
-    message = f"{date} | {username} > {message_send}"
+    message = f"{date} ; user: {username} ; {message_send}"
     client_socket.send(message.encode())
 
 
--- server.py
+++ server.py
@@ -32,10 +32,67 @@ print("Listening for connections on 127.0.0.1:%d" %(server_port),"...")
 
 # Infinite loop to listen to clients
 while True:
-    # Get any clients and accept their connections
+    # Accept connections from potential clients
     client_socket, client_addr = server_socket.accept()
-    clients.append(client_socket)
-    print(f"Accepting new connection from {client_addr}")
+    
+    # Check if client's details are true. Client gets 3 attempts before being locked out
+    # Check for username
+    while True:
+        credentials_username = client_socket.recv(1024).decode()
+        break
+
+    # Iterate through credentials file to check for username
+    credientials_file = open("credentials.txt", "r+")
+    for value in credientials_file:
+        crediential = value.split()
+        # If username exists, get password from client
+        if crediential[0] == credentials_username:
+            client_socket.send(("username_validated").encode())
+            #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! PRNTING USERS PASSWORDS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            print(crediential[1])
+
+            # Check if password matches username
+            attempts = 2
+            while True:
+                # Get password from client
+                credentials_password = client_socket.recv(1024).decode()
+
+                # Check if password matches
+                if crediential[1] == credentials_password:
+                    client_socket.send(("user_authorised").encode())
+                                    
+                    # Add client to list of clients
+                    clients.append(client_socket)
+                    print(f"Accepting new connection from {crediential[0]}")
+                    break 
+
+                # Check if the client has not attempted to login more that 3 times
+                elif attempts > 0:
+                    client_socket.send(("Error: Incorrect password. Attempts remaining {attempts}").encode())
+                    attempts = attempts - 1
+                # Otherwise break connection with the client
+                else: 
+                    client_socket.send(("Error: Incorrect password. Breaking connection with this client").encode())
+                    break
+
+        # If username does not exist, create a new account
+        else:
+            client_socket.send(("username_invalid").encode())
+            credentials_password = client_socket.recv(1024).decode()
+            new_credntials = credentials_username + ' ' + credentials_password
+            # Append new account to credentials file
+            credientials_file.write("\n")
+            credientials_file.write(new_credntials)
+
+            # Add client to list of clients
+            clients.append(client_socket)
+            print(f"Accepting new connection from {credentials_username}")
+            
+            # Let client know, account creation was successful
+            client_socket.send(("account_created").encode())
+            break
+    # Close credentials file
+    credientials_file.close()
 
     # Recieve/listen for messages sent by client
     while True:
